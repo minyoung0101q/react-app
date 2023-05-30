25150082d78d7fe5fafc2bfbacad3378164c2e9e
stylish_code <130943387+minyoung0101q@users.noreply.github.com>
2023-05-31 02:14:36 +0900

    이전 두 개 방식들 말고 이것이 리액트에서 css 파일 활용방법! === 같은 클래스 이름을 사용해도 문제 되지 X! === 컴포넌트들이나 스타일들을 독립적이게 유지시켜줌 === create-react-app 이야기는 끝 === 다음 코스는 ReactJS의 마지막 커다란 컨셉 =>암호화폐 어플리케이션 구현(암호화폐 가격을 가져와서 사람들에게 보여줄 것) => 이것을 하기 위해서는 컴포넌트의 마지막 한 조각을 배워야 함 effect에 대해 코딩할 예정.

--- src/Button.js
+++ src/Button.js
@@ -1,16 +1,8 @@
 import PropTypes from "prop-types";
+import styles from "./Button.module.css";
 
 function Button({ text }) {
-  return (
-    <button
-      style={{
-        backgroundColor: "tomato",
-        color: "white",
-      }}
-    >
-      {text}
-    </button>
-  );
+  return <button className={styles.title}>{text}</button>;
 }
 
 Button.propTypes = {
