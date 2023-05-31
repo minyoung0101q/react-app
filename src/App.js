14033112d5f6c2a915b36e7f46c9fa168997b220
stylish_code <130943387+minyoung0101q@users.noreply.github.com>
2023-05-31 15:54:43 +0900

    useEffect-2 state가 변경될 때마다 코드 전체가 다시 실행되는 형태(i run all the time)와 staet가 변경될 때 처음 한 번만 실행되고 그 이후는 실행되지 않는 형태(i run only once)!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import { useState } from "react";
-
+import {
+  useState,
+  useEffect,
+} from "react"; /*useEffect는 두 개의 argument를 가지는 function
+첫 번째 argument : 딱 한 번만 실행하고 싶은 코드
+두 번째 argument : 나중에 할 것
+api를 호출하는 것과 같은 중요한 상황일 때, state가 변경되면 처음 한 번만 코드 실행, 그 이후 다시는 실행되지 않게 문제 해결부터!
+*/
 function App() {
   const [counter, setValue] = useState(0); //setValue는 counter를 변경해주는 함수
   const onClick = () => setValue((prev) => prev + 1); //이전 counter에 1이 더해져서 return 됨
-  console.log("call an api");
+  console.log("i run all the time");
+  const iRunOnlyOnce = () => {
+    console.log("i run only once.");
+  };
+  useEffect(iRunOnlyOnce, []);
   return (
     <div>
       <h1>{counter}</h1>
