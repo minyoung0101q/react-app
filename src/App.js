52a59d488839b5713be45b45776d7116e89353b2
stylish_code <130943387+minyoung0101q@users.noreply.github.com>
2023-05-31 17:51:30 +0900

    Deps-5 useEffect의 첫번째 인자와 두번째 인자 정리 === strictmode 삭제 코드에서 주석 확인 === 다음 커밋에서는 /* 하지만, 아직 검색창이 완벽하게 동작한다고 할 수는 없다! 그건 component가 시작될 때도 검색이 되고 있기 때문이다. => 이건 다음 커밋에서 확인. */ if문을 사용해 검색 부분의 코드를 고쳐볼 것

--- src/App.js
+++ src/App.js
@@ -28,6 +28,48 @@ function App() {
 
 export default App;
 
-/* 코드 확인 : keyword가 변화할 때만 marvel을 검색하도록 할 것!
-  이 'keyword'가 변화할 때 코드를 실행할 거라고 react.js에게 알려주는 것
+/* 
+이번엔 counter가 변화할 때에는 console.log("SEARCH FOR", keyword); => 이 코드는 실행되지 X.
+위 코드는 keyword가 변화할 때만 실행됨.
+이제 useEffect의 첫 번째 인자와 두 번째 인자를 쓰는 방식을 이해함
+
+정리하자면 다음과 같다.
+우리가 useEffect의 첫번째 인자만 써주고, 두번째 인자를 빈 array를 준다면([]) => 코드는 단 한 번만 실행된다.
+9줄에서는 react가 지켜볼 게 아무것도 없으니까(두번째 인자가 빈 배열이니까) 처음 한 번만 실행된다.
+12줄에서는 keyword가 변화할 때 마다 코드가 항상 실행된다. => 이건 우리가 react.js에게 '이 keyword를 지켜봐.'라고 말해줬기 때문!!
+***즉, keyword가 변화할 때 코드를 실행하라는 말이다!!***
+(1)
+크롬에서 새로고침하면!
+예상대로 처음에는 모든 게 실행된다.
+i run all the time
+CALL THE API....
+SEARCH FOR
+위가 console 창에 뜬다.
+
+(2)
+만약 click me 버튼을 클릭하면
+계속 실행되고자 했던 코드만 실행되고,
+검색하는 movies API는 호출되지 않는 것을 볼 수 있다!!
+i run all the time => 이것만 클릭한 횟수만큼 console 창에 뜬다.
+
+(3)
+만약 영화를 검색한다면 => input 창에 키보드로 입력하는 것을 의미
+이제는 영화를 검색하고 있다!
+예를 들어 m 을 치면 => console창에는 
+i run all the time
+SERACH FOR m
+위와 같이 뜬다!
+
+이것이 useEffect의 마법이다!
+*/
+
+/* 
+하지만, 아직 검색창이 완벽하게 동작한다고 할 수는 없다!
+그건 component가 시작될 때도 검색이 되고 있기 때문이다.
+=> 이건 다음 커밋에서 확인.
+*/
+
+/*
+StrictMode 제거하면 console.log가 두 번 찍히는 것을 방지 => 아래 블로그 참고
+: https://velog.io/@hyes-y-tag/React-useEffect%EA%B0%80-%EB%91%90%EB%B2%88-%EC%8B%A4%ED%96%89%EB%90%9C%EB%8B%A4%EA%B3%A0
 */
