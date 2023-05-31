9207246dac21e49d1f302d111eee3fc67e26e93a
stylish_code <130943387+minyoung0101q@users.noreply.github.com>
2023-05-31 16:15:06 +0900

    Deps input과 state의 콤비네이션 keyword staet와 onChange function 생성 onChange function은 우리의 input에 event listener로 들어갈 것. onChange가 발생하면 const onChange() => 괄호 안에서 그 event를 받는다. 그리고 input에 value를 줘서 state와 연결시켜줌! ===> 이전에 배운 내용 input과 state combination. === 여기서 useEffect를 사용해야 하는 이유를 알 수 있다. 누군가 글자를 타이핑할 때 마다 API를 새로 호출한다고 생각해보면 완전 별로다. 현재는 검색할 때마다 계속 코드를 전체 재실행 중이다.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import {
-  useState,
-  useEffect,
-} from "react"; /*useEffect는 두 개의 argument를 가지는 function
-첫 번째 argument : 딱 한 번만 실행하고 싶은 코드
-두 번째 argument : 나중에 할 것
-api를 호출하는 것과 같은 중요한 상황일 때, state가 변경되면 처음 한 번만 코드 실행, 그 이후 다시는 실행되지 않게 문제 해결부터!
-*/
+import { useState, useEffect } from "react";
 function App() {
   const [counter, setValue] = useState(0); //setValue는 counter를 변경해주는 함수
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1); //이전 counter에 1이 더해져서 return 됨
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("i run all the time");
   useEffect(() => {
     console.log("CALL THE API....");
   }, []);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
@@ -25,3 +26,4 @@ export default App;
 
 /* useEffect function은 쉽게 말해서
 우리 코드가 딱 한번만 실행될 수 있도록 보호해준다! */
+/* 저번에 했던 state와 input의 콤비네이션을 연습할 것! */
