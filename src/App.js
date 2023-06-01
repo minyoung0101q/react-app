45ed57f3e434592d3d6a5bb56198f2b9ec2c8538
stylish_code <130943387+minyoung0101q@users.noreply.github.com>
2023-06-01 16:56:54 +0900

    Cleanup-4 이전 커밋에서는 Cleanup function 작성을 해봄 === 코드 설명: React.js가 effectFn을 실행할 거고, component가 파괴될 때는 react.js는 hiFn이 return한 function을 실행할 것. ====여기 까지가 끄읕 정리: useEffect는 유용함, 우리에게 선택권을 제공해줌 시작할 때만 code를 실행할 수 있게 하고, 무언가가 변화할 때 code가 실행되도록 할 수도 있고, (이번에 배운 것) component가 파괴될 때 code를 실행할 수도 있음!!

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import { useState, useEffect } from "react";
 
 function Hello() {
-  useEffect(() => {
+  function destroyedFn() {
+    console.log("bye :(");
+  }
+  function effectFn() {
     console.log("created :)");
-    return () => console.log("destroyed :("); //Cleanup function이라고 부르는 것!
-  }, []);
+    return destroyedFn;
+  }
+  useEffect(effectFn, []); //component가 파괴될 때 function을 실행하고 싶으면, effectFn이 새로운 function을 return해야 함!
   return <h1>Hello</h1>;
 }
 
 function App() {
   const [showing, setShowing] = useState(false);
   const onClick = () => setShowing((prev) => !prev); //setShowing을 통해 이전 value를 받아온 다음에, 그 value의 반댓값을 return 할 것
-  console.log("항상 실행");
+
   return (
     <div>
       {showing ? <Hello /> : null}{" "}
@@ -25,6 +29,14 @@ function App() {
 
 export default App;
 
-/* Cleanup function : 많이 쓰이는 것은 아님!
+/* react 경력자가 말하건데,
+react 앱을 만들 때 destroyedFn이 주로 필요하지는 않다.
+cleanup function을 사용하지 않는다고 함
+이게 그렇게 자주 일어날 일은 아님
+가끔은 해야 할 필요가 있을 수도 있지만,,
+할 수 있는 것 자체는 좋다.
 
+그러니까 실제로 destroyedFn 같은 것을 어디에 적용할 지 떠오르지 않는다고 해도
+걱정하지 않아도 된다.
+정말 특정한 케이스에만 사용하게 될 것이니까!
 */
