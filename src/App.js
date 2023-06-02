c9ebe4509be89f485963f4c48d72c7bae8e3a2eb
stylish_code <130943387+minyoung0101q@users.noreply.github.com>
2023-06-02 16:58:38 +0900

    Coin Tracker 구현 : 암호화폐들과 가격을 나열. 구현 위해 useEffect 연습. 페이지나 앱을 들어왔을 때 로딩 메세지가 보이고 코인들이 나열되면  로딩 메세지를 숨기고 코인들을 멋진 리스트로 보여주는 것.

--- src/App.js
+++ src/App.js
@@ -1,51 +1,17 @@
-import { useState, useEffec } from "react";
+import { useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return; //만약 toDO가 비어있다면 그냥 return 시킴 -> 이 함수를 작동하지 않도록 만듦
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo(""); //그렇지 않다면 toDo를 추가하도록 만듦
-  };
-  console.log(toDos);
-  console.log(toDos.map((item, index) => <li key={index}>{item}</li>));
+  const [loading, setLoading] = useState(true);
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>{" "}
-      {/*jsx에서 자바스크립트를 넣고 싶다면 { 중괄호 안에 작성 }*/}
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-        {/* toDos는 text로 된 array */}
-      </ul>
+      <h1>The Coins!</h1>
+      {loading ? <strong>Loading...</strong> : null}
     </div>
   );
 }
 
 export default App;
 
-/* map 함수
-map은 하나의 array에 있는 item을 내가 원하는 무엇이든지로 바꿔주는 역할을 하고
-그건 결국 새로운 array로 return 해줌.
-
-map은 함수의 첫번째 argument로 현재의 item을 가져올 수 있음.
-map의 함수의 첫번째 argument가 진행되고 있는 순서에 맞는 item임.
-
-map을 활용해 component를 return 한다!!
+/* Coin Tracker
+State 생성 => 1. 하나는 로딩을 위한 거고 2. 또 다른 건 리스트를 잠시 갖고 있기 위한 것
 */
